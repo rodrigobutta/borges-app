b44185ddeb575d3265214eabe9bad92cd1ea2f4e
rodrigobutta <rbutta@gmail.com>
2022-05-10 00:15:09 -0300

    messing up with libs

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,18 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@': './src',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@': './src',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
